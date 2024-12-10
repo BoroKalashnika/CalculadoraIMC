d40410317830b9e86157f95d99b0a57612c68baa
BoroKalashnika <boromir111bb@gmail.com>
2024-12-10 10:15:47 +0100

    css improved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-/**
- * 
- * @format
- */
-
 import React, { useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { StyleSheet, View } from 'react-native';
-import Resultat from './components/Resultat'
-import Calculadora from './components/Calculadora'
-import Titol from './components/Titol'
+import Resultat from './components/Resultat';
+import Calculadora from './components/Calculadora';
+import Titol from './components/Titol';
 
 function App() {
   const [imc, setImc] = useState('');
@@ -23,17 +18,21 @@ function App() {
 
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <View style={styles.title}>
-          <Titol />
-        </View>
-        <View style={styles.input}>
-          <Calculadora onCalcIMC={actualizarValores} />
-        </View>
-        <View style={styles.button}>
-          {show && (<Resultat valImc={imc} valTxtImc={txtImc} />)}
-        </View>
-      </View>
+      <KeyboardAvoidingView style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.title}>
+              <Titol />
+            </View>
+            <View style={styles.input}>
+              <Calculadora onCalcIMC={actualizarValores} />
+            </View>
+            <View style={styles.button}>
+              {show && <Resultat valImc={imc} valTxtImc={txtImc} />}
+            </View>
+          </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </PaperProvider>
   );
 }
@@ -42,15 +41,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+  },
   title: {
-    flex: 1,
+    flex: 0.5,
   },
   input: {
     flex: 1,
   },
   button: {
-    flex: 1,
-  }
+    flex: 10,
+  },
 });
 
 export default App;
